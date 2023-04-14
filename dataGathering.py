edcfc6f977482e93da4a3e46d3e92aa4c4725aa6
mu11ern <mu11ern@yahoo.com>
2023-04-14 12:19:06 -0700

    added print msg to script because process is long

--- dataGathering.py
+++ dataGathering.py
@@ -1,14 +1,25 @@
 import urllib3
 import datetime
-
 http = urllib3.PoolManager()
 todaysDate = datetime.date.today().strftime("%m-%d-%Y-")
 
-resp = http.request('GET' , 'http://psudataeng.com:8000/getBreadCrumbData')
-print(resp.status)
-# print(resp.data)
+print("Reaching out to TriMet data source .. ", end='', flush = True)
 
-if(resp.status == 200):
-    with open(todaysDate + 'TriMet.txt', 'wb') as writeOut:
-        writeOut.write(resp.data)
-    writeOut.close()
+def requestTrimetData():
+    return(http.request('GET' , 'http://psudataeng.com:8000/getBreadCrumbData'))
+    # print(resp.status)
+    # print(resp.data)
+
+def TriMetToText(resp):
+    if(resp.status == 200):
+        print('Successful!')
+        print("New file "+ todaysDate+"TriMet.txt created.")
+        with open(todaysDate + 'TriMet.txt', 'wb') as writeOut:
+            writeOut.write(resp.data)
+        writeOut.close()
+    else:
+        print('Unsuccessful ..')
+        print('Something went wrong, no new file created.')
+
+resp = requestTrimetData()
+TriMetToText(resp)
